b7d1e8d7d1891ff158b6f1e7023d726d5ba1fb5d
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-02-28 10:09:05 -0300

    Revert "Index function added"
    This reverts commit 13a5ec77624c2f54b05d8c12f0d94b78833e75e9.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index(){
-        $posts = Post::latest()->get();
-
-        return view('posts.index', compact('posts'));
-    }
-    
+    //
 }
