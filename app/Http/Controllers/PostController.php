13a5ec77624c2f54b05d8c12f0d94b78833e75e9
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-02-28 10:08:38 -0300

    Index function added

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        $posts = Post::latest()->get();
+
+        return view('posts.index', compact('posts'));
+    }
+    
 }
