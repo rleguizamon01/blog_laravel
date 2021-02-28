b17d73b222c84bfa1ff599ec0ff8d7be655be9b8
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-02-28 09:21:48 -0300

    Relationship from post to category added

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,8 @@ class Post extends Model
     public function user(){
         return $this->belongsTo(Post::class);
     }
+
+    public function category(){
+        return $this->hasOne(Category::class);
+    }
 }
